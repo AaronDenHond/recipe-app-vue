42013b39eb76902152e333818a6313345a9c23c2
Aaron <aaron.denhond@gmail.com>
2021-09-22 16:05:05 +0200

    boilerplate,html, 2way form binding, addRecipe method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <!-- submit.prevent wont reload the page -->
+  <form @submit.prevent="addRecipe">
+    <div>
+      <label>Name</label><br />
+      <!-- v-model is vue's way of 2way binding input data to model -->
+      <input v-model="recipe.name" type="text" name="" id="" />
+    </div>
+    <div>
+      <label>Ingredients</label><br />
+      <textarea
+        v-model="recipe.ingredients"
+        name=""
+        id=""
+        cols="30"
+        rows="5"
+      ></textarea>
+    </div>
+    <div>
+      <label>Instructions</label><br />
+      <textarea
+        v-model="recipe.instructions"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+    <button type="submit">Add Recipe</button>
+  </form>
+  <!-- v-for is vues way of foreach loping -->
+  <div v-for="recipe of recipes" v-bind:key="recipe.id">
+    <p>{{recipe.name}}</p>
+    <p>{{recipe.ingredients}}</p>
+    <p>{{recipe.instructions}}</p>
+    
+   
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      recipe: {
+        name: "",
+        ingredients: "",
+        instructions: "",
+      },
+      recipes: [],
+    };
+  },
+  methods: {
+    addRecipe() {
+      this.recipes.push(this.recipe);
+      console.log(this.recipes)
+      
+    },
+    
+  },
+  
+};
+
 </script>
 
 <style>
